8cfd06a918356b5ad6c32f5cf7e0b79c4fa17ab1
Zakaria Makhkhas <zmakhkha@e3r3p9.1337.ma>
2024-07-30 14:04:12 +0100

    merging

---
+++ back/run.sh
@@ -0,0 +1,8 @@
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py flush --no-input
+
+python manage.py runserver 0.0.0.0:2000
