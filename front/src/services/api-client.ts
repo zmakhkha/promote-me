2e362af4255d530d51c5004f761d2563598801dd
Zakaria Makhkhas <zmakhkha@e3r1p10.1337.ma>
2024-08-09 08:48:54 +0100

    added token expiration logic

--- front/src/services/api-client.ts
+++ front/src/services/api-client.ts
@@ -25,4 +25,15 @@ apiClient.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      window.location.href = '/login';
+    }
+    
+    return Promise.reject(error);
+  }
+);
+
 export default apiClient;
